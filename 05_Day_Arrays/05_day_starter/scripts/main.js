06ce49abbb8b5cd60813ef6dbb78baf765472040
Nicole Gray <ngray122@gmail.com>
2021-02-28 19:05:17 -0500

    ngray/arrays

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,59 @@
+//1.
+const emptyArray = [];
+
+//2.
+const favNumbers = [2, 6, 8, 67, 44];
+
+//3.
+favNumbers.length;
+
+//4.
+console.log(favNumbers[0]);
+console.log(favNumbers[2]);
+console.log(favNumbers[5]);
+
+//5.
+const mixedDataTypes = [
+    48, 
+    "Hello", 
+    false, 
+    {name: "Nicole", age: "34"}, 
+    null];
+
+console.log(mixedDataTypes);
+console.log(mixedDataTypes.length);
+
+//6.
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+//7.
+console.log(itCompanies);
+
+//8.
+console.log(itCompanies.length);
+
+//9.
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[6]);
+
+//10. 
+
+//12.
+itCompanies.toString();
+
+//15.
+itCompanies.sort();
+
+//16.
+itCompanies.reverse();
+
+//17.  console prints that 
+itCompanies.slice(0, 3);
+
+//18.
+itCompanies.splice(3,7);
+
 console.log(countries)
 alert('Open the browser console whenever you work on JavaScript')
 alert('Open the console and check if the countries has been loaded')
