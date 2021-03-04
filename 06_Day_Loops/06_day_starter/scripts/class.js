7363cb37cad77e8ef685084aee0744f51b2e2022
Nicole Gray <ngray122@gmail.com>
2021-03-04 18:48:04 -0500

    Loops Excercise 1 1-2

---
+++ 06_Day_Loops/06_day_starter/scripts/class.js
@@ -0,0 +1,48 @@
+//Excercise 1
+
+//1.
+for (let i = 0; i <= 10; i++) {
+  console.log(i)
+};
+
+
+let i = 0;
+while (i <= 10){
+  console.log(i)
+  i++
+};
+
+
+let i = 0;
+do {
+  console.log(i)
+  i++
+} while (i <= 10);
+
+
+//2.
+for (let i = 10; i >= 0; i-- ){
+    console.log(i) 
+};
+
+
+let i = 10;
+while (i >= 0){
+  console.log(i)
+  i--
+};
+
+
+let i = 10;
+do {
+  console.log(i)
+  i--
+} while(i >= 0);
+
+
+//3. 
+
+//4.
+
+
+
