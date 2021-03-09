ec54d4922b9ed1bcd5b17634c4aae2ce051bd44c
Nicole Gray <ngray122@gmail.com>
2021-03-08 19:42:58 -0500

    loops 1-8

--- 06_Day_Loops/06_day_starter/scripts/class.js
+++ 06_Day_Loops/06_day_starter/scripts/class.js
@@ -43,6 +43,25 @@ do {
 //3. 
 
 //4.
+for (let i = 0; i <= 6; i++){
+  console.log('#')
+};
+
+//5. 
+for (let i = 0; i <= 10; i++){
+  console.log(i * i)
+};
+
+//6.
+for (let i = 0; i <= 10; i++){
+  console.log(i * i * i)
+};
+
+//7. 
 
 
-
+//8.
+for (let i = 0; i <= 100; i++){
+  if (i % 2 !== 0)
+  console.log(i)
+};
